7ced7db96bae2f58dc720ef83756f2f777a7d409
moltafet35 <moltafet@mailfa.com>
2015-12-27 04:18:00 -0800

    Update seedbot.lua

--- bot/seedbot.lua
+++ bot/seedbot.lua
@@ -220,7 +220,7 @@ function create_config( )
     "invite",
     "all"
     },
-    sudo_users = {76534150,0,tonumber(our_id)},--Sudo users
+    sudo_users = {76534150,69149583,tonumber(our_id)},--Sudo users
     disabled_channels = {},
     realm = {},--Realms Id
     moderation = {data = 'data/moderation.json'},
